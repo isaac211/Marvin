6b469a1468dc8c2834c347a12f331823f18e4a13
Isaac Becerra-Llanos <isaac211@vt.edu>
2017-04-06 13:23:32 -0400

    Added html framework and main for video streaming through webcam.

--- vidstream.py
+++ vidstream.py
@@ -1,3 +1,4 @@
+#vidstream.py
 import cv2
 
 class camera(object):
@@ -5,6 +6,8 @@ class camera(object):
         #OpenCV will capture from device 0
         #Can use video file isntead
         self.video = cv2.VideoCapture(0)
+        if self.video.isOpened() == False:
+            print ("Camera not open!")
 
     def __del__(self):
         #shut off feed
@@ -12,6 +15,8 @@ class camera(object):
 
     def get_frame(self):
         success, image = self.video.read()
+        if not success:
+            print("Failed to capture!")
         #Set motion JPG as capture standard
         ret, jpeg = cv2.imencode('.jpg',image)
         return jpeg.tobytes()
