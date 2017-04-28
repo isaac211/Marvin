bab1a22f76323dce76cd61b54b0829f85238e078
TechnoSam <technosam4jc@gmail.com>
2017-04-28 18:59:23 -0400

    Setup WebSockets from client to server
    For some reason I can't go the other way

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO, emit
+
+import sys
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     #specify local web page
     return render_template('index.html')
 
+@socketio.on('connect')
+def connect():
+    print("Socket Connected");
+    sys.stdout.flush()
+    #send('event', 'Connected')
+
+@socketio.on('event')
+def event(msg):
+    print(msg);
+    sys.stdout.flush()
+
+@socketio.on('disconnect')
+def disconnect():
+    print("Client disconnected")
+
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0')
--- static/main.js
+++ static/main.js
@@ -1,26 +1,57 @@
 
+var commsocket;
+
 $(document).ready(function() {
     console.log("Script Loaded");
+    commsocket = io.connect("http://127.0.0.1:5000/");
+    commsocket.on('connect', function() {
+        //socket.emit('my_event', {data: 'I\'m connected!'});
+        console.log("Socket Connected");
+    });
+    commsocket.on('event'), function(msg) {
+        console.log("msg");
+        console.log(msg);
+    }
 });
 
 function up() {
     msgOut("Up pressed!");
     console.log("Up");
+    var upMsg = {
+        type: "move",
+        direction: "forward"
+    };
+    commsocket.emit('event', upMsg);
 }
 
 function down() {
     msgOut("Down pressed!");
     console.log("Down");
+    var downMsg = {
+        type: "move",
+        direction: "backward"
+    };
+    commsocket.emit('event', downMsg);
 }
 
 function left() {
     msgOut("Left pressed!");
     console.log("Left");
+    var leftMsg = {
+        type: "move",
+        direction: "left"
+    };
+    commsocket.emit('event', leftMsg);
 }
 
 function right() {
     msgOut("Right pressed!");
     console.log("Right");
+    var rightMsg = {
+        type: "move",
+        direction: "right"
+    };
+    commsocket.emit('event', rightMsg);
 }
 
 function indTogg() {
