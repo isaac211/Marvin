bf97941e0f27a28bc0910538ef3c7a3d4673161a
TechnoSam <smcfalls@vt.edu>
2017-04-28 20:57:30 +0000

    Remove video code
    With the video code out of master, it will be possible to develop
    on Pis that do not have OpenCV.
    Replaced stream with a cute image of Marvin saying Don't Panic.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, Response
-from vidstream import camera
 
 app = Flask(__name__)
 
@@ -7,17 +6,6 @@ app = Flask(__name__)
 def index():
     #specify local web page
     return render_template('index.html')
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(camera()),
-            mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
